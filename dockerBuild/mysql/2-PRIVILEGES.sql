68bb9514fff8006bc96bc50a3074484ed026d2fe
Cédric Cousseran <cedric.cousseran@gmail.com>
2016-03-01 11:21:01 +0100

    Move dockerfiles to another github

--- dockerBuild/mysql/2-PRIVILEGES.sql
+++
@@ -1,9 +0,0 @@
-  #-----------------------------------
-  #USER RIGHTS MANAGEMENT
-  #-----------------------------------
-  CREATE USER 'admincdb' IDENTIFIED BY 'qwerty1234';
-
-  GRANT ALL PRIVILEGES ON `computer-database-db`.* TO 'admincdb' WITH GRANT OPTION;
-
-
-  FLUSH PRIVILEGES;
