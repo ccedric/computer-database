0fc1042aa2704274e25371141237fa6650c854e4
klaurent <klaurent@excilys.com>
2016-04-11 11:51:08 +0200

    Ajout du use computer-database-db + fix erreur script

--- db/createSpringSecurityTables.sql
+++ db/createSpringSecurityTables.sql
@@ -1,13 +1,15 @@
-CREATE  TABLE user (
+use computer-database-db;
+
+CREATE TABLE if not exists user (
   id bigint not null auto_increment,
   username VARCHAR(45) NOT NULL ,
   password VARCHAR(60) NOT NULL ,
   enabled TINYINT NOT NULL DEFAULT 1 ,
   PRIMARY KEY (id));
 
-CREATE TABLE user_role (
+CREATE TABLE if not exists user_role (
   id bigint not null auto_increment,
   user_id bigint not null,
   role varchar(45) NOT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT fk_users_id FOREIGN KEY (user_id) REFERENCES users (id));
+  CONSTRAINT fk_users_id FOREIGN KEY (user_id) REFERENCES user (id));
